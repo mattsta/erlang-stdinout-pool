a057c35da62edf9e8ccedd67820656690ed2b12d
Matt Stancliff <matt@genges.com>
2011-02-07 19:33:52 -0800

    Add function to use network API from Erlang

--- src/stdinout.erl
+++ src/stdinout.erl
@@ -1,8 +1,9 @@
 -module(stdinout).
 
-%% gen_server callbacks
--compile(export_all).
+-export([start_link/2, start_link/4, start_link/5]).
 
+-export([send/2, send/3]).
+-export([shutdown/1]).
 %%====================================================================
 %% Starting
 %%====================================================================
@@ -21,8 +22,21 @@ start_link(GenServerName, Cmd, IP, Port, SocketCount) ->
 send(Server, Content) ->
   gen_server:call(Server, {stdin, Content}).
 
-%%====================================================================
+send(Host, Port, Content) ->
+  {ok, Sock} = gen_tcp:connect(Host, Port, [binary, {active, false}]),
+  Length = integer_to_list(iolist_size(Content)),
+  Formatted = [Length, "\n", Content],
+  gen_tcp:send(Sock, Formatted),
+  recv_loop(Sock, []).
+
+recv_loop(Sock, Accum) ->
+  case gen_tcp:recv(Sock, 0) of
+          {ok, Bin} -> recv_loop(Sock, [Bin | Accum]);
+    {error, closed} -> lists:reverse(Accum)
+  end.
+
+%%===================================================================
 %% Stopping
 %%====================================================================
 shutdown(Server) ->
-  Server ! shutdown.
+  gen_server:call(Server, shutdown).
